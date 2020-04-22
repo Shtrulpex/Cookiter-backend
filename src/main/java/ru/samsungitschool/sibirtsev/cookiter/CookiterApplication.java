c65ff96677f28f81c76244b0efb627caf4c5ad24
Shtrulpex <issibirtsev@gmail.com>
2020-04-22 17:29:27 +0300

    init2

--- src/main/java/ru/samsungitschool/sibirtsev/cookiter/CookiterApplication.java
+++ src/main/java/ru/samsungitschool/sibirtsev/cookiter/CookiterApplication.java
@@ -2,6 +2,7 @@ package ru.samsungitschool.sibirtsev.cookiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CookiterApplication {
@@ -9,5 +10,8 @@ public class CookiterApplication {
     public static void main(String[] args) {
         SpringApplication.run(CookiterApplication.class, args);
     }
-
+    @RequestMapping("/")
+    public String index() {
+        return "!";
+    }
 }
