4b5cbf2ae2e11d9dd392c5ca22b8eb8b163914a4
Shtrulpex <issibirtsev@gmail.com>
2020-05-29 11:57:32 +0300

    0.2.3

--- src/main/java/ru/samsungitschool/sibirtsev/cookiter/mappers/RecipesMapper.java
+++ src/main/java/ru/samsungitschool/sibirtsev/cookiter/mappers/RecipesMapper.java
@@ -7,6 +7,7 @@ import java.sql.Array;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class RecipesMapper implements RowMapper<Recipes> {
     public Recipes mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -16,8 +17,14 @@ public class RecipesMapper implements RowMapper<Recipes> {
         recipe.setRecipe(rs.getString("recipe"));
         recipe.setId(rs.getInt("id"));
         String ar = rs.getArray("products").toString();
-        recipe.setAr(ar);
-       // recipe.setProducts((Integer[])ar.getArray());
+
+        ar=ar.substring(1,ar.length()-1);
+        String[] a = ar.split(",");
+        Integer[] array = new Integer[a.length];
+        for(int i=0;i<a.length;i++){
+            array[i]=Integer.parseInt(a[i]);
+        }
+        recipe.setProducts(array);
         return recipe;
     }
 }
